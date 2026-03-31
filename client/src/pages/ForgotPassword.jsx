7eb4984f533ae93eeed8fb5b6f61f54d6b1045ee
Dipak Singh <dipaksingh3323@gmail.com>
2026-03-31 17:46:13 +0530

    today i have done forgot UI page

---
+++ client/src/pages/ForgotPassword.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
+const ForgotPassword = () => {
+  const [step, setStep] = useState(3);
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const navigate = useNavigate()
+  return (
+    <div className="flex w-full items-center justify-center min-h-screen p-4 bg-[#fff9f6]">
+      <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-8">
+        <div className="flex items-center gap-4 mb-4 cursor-pointer" onClick={()=>navigate("/signin")}>
+          <IoMdArrowRoundBack size={30} className="text-[#ff4d2d]" />
+          <h1 className="text-2xl font-bold text-center text-[#ff4d2d]">
+            Forgot Password
+          </h1>
+        </div>
+         {/* Step 1 */}
+        {step == 1 && (
+          <div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 font-medium mb-1"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="w-full border-[1px] border-gray-200 rounded-lg px-3 py-2 focus:outline-none"
+                placeholder="Enter Your Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div>
+            <button
+          className={`w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 cursor-pointer bg-[#ff4d2d] text-white hover:bg-[#e64323]`}
+        >
+          Send OTP
+        </button>
+          </div>
+        )}
+        {/* Step 2 */}
+        {step==2 && 
+        <div>
+           <div className="mb-6">
+              <label
+                className="block text-gray-700 font-medium mb-1"
+                htmlFor="email"
+              >
+                OTP
+              </label>
+              <input
+                type="email"
+                className="w-full border-[1px] border-gray-200 rounded-lg px-3 py-2 focus:outline-none"
+                placeholder="Enter OTP"
+                onChange={(e) => setOtp(e.target.value)}
+                value={otp}
+              />
+            </div>
+            <button
+          className={`w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 cursor-pointer bg-[#ff4d2d] text-white hover:bg-[#e64323]`}
+        >
+          Verify Otp
+        </button>
+          </div>
+          }
+
+           {/* Step 3 */}
+
+           {step==3 && 
+        <div>
+           <div className="mb-6">
+              <label
+                className="block text-gray-700 font-medium mb-1"
+                htmlFor="newPassword"
+              >
+                New Password
+              </label>
+              <input
+                type="email"
+                className="w-full border-[1px] border-gray-200 rounded-lg px-3 py-2 focus:outline-none"
+                placeholder="Enter New Password"
+                onChange={(e) => setNewPassword(e.target.value)}
+                value={newPassword}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 font-medium mb-1"
+                htmlFor="confirmPassword"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="email"
+                className="w-full border-[1px] border-gray-200 rounded-lg px-3 py-2 focus:outline-none"
+                placeholder="Enter Confirm Password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+              />
+            </div>
+            <button
+          className={`w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 cursor-pointer bg-[#ff4d2d] text-white hover:bg-[#e64323]`}
+        >
+          Reset Password
+        </button>
+          </div>
+          }
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
